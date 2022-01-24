323fa0acedd6ec2e8f90103476d5cd1edca807c4
zhaozhuobin <350243725@qq.com>
2022-01-24 16:43:25 +0800

    [Feature]update时增加plugin的update机制

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -6,35 +6,64 @@ import lpminit from '../util/lpminit'
 export default class Update extends Command {
   static description = '更新lisa到最新版本'
 
+  static args = [
+    {
+      name: 'plugin',
+      required: false,
+      description: "拓展包名，例：'zephyr'，不填时默认更新lisa自身",
+    },
+  ]
+
   async run() {
-    const {cli, cmd} = lisa
-    cli.action.start('正在更新到最新版本...')
+    const {cli, cmd, application} = lisa
+    cli.action.start('更新启动...')
     const DEBUG = process.env.LISA_ENV === 'debug'
-    const nowVersion = this.config.version
-    this.debug('当前版本 %s', nowVersion)
-    await lpminit()
+    const {args} = this.parse(Update)
+
+
+
+    let wantUpdatePkg = '@listenai/lisa'
+    let registry = ''
+    let command = []
+    if (args.plugin) {
+      wantUpdatePkg = `@lisa-plugin/${args.plugin}`
+      await lpminit()
+      registry = `--registry=${application.registryUrl}`
+    }
 
     if (DEBUG) {
-      this.log('当前环境为 beta，将安装更新beta版本')
-      await cmd('npm', ['install', '@listenai/lisa@beta', '-g'])
+      this.log(`当前环境为 beta，将更新到${wantUpdatePkg}的beta版本`)
+      command = ['install', `${wantUpdatePkg}@beta`, '-g']
+      await cmd('npm', registry ? command.concat(registry) : command, {
+        shell: true,
+        stdio: 'inherit',
+      })
       return
     }
 
-    const res = await cmd('npm', ['view', '@listenai/lisa', 'dist-tags'])
+    const nowVersion = this.config.version
+    this.debug('当前版本 %s', nowVersion)
 
     try {
+      command = ['view', wantUpdatePkg, 'dist-tags']
+      const res = await cmd('npm', registry ? command.concat(registry) : command)
       const distTags = JSON.parse(res.stdout.replace(/(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)(['"])?:/g, '$1"$3":').replace(/'/g, '"'))
       const latestVersion = distTags.latest
       this.debug('最新版本 %s', latestVersion)
 
-      if (compare(nowVersion, latestVersion) >= 0) {
+      if (wantUpdatePkg === '@listenai/lisa' && compare(nowVersion, latestVersion) >= 0) {
         return cli.action.stop(`已是最新版本 ${nowVersion}`)
       }
 
-      await cmd('npm', ['install', '@listenai/lisa@latest', '-g'])
-      cli.action.stop('成功')
+      cli.action.stop(`开始更新${wantUpdatePkg}@${latestVersion}`)
+      command = ['install', `${wantUpdatePkg}@latest`, '-g']
+      await cmd('npm', registry ? command.concat(registry) : command, {
+        shell: true,
+        stdio: 'inherit',
+      })
+      this.log('成功')
     } catch (error) {
-      this.debug(error)
+      throw error
     }
   }
 }
