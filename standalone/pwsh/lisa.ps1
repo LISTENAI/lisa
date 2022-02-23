143bc83044803de3e03acd14e7b631f5d903f68d
zhaozhuobin <350243725@qq.com>
2022-02-23 20:37:52 +0800

    暂将check-update去掉

--- standalone/pwsh/lisa.ps1
+++ standalone/pwsh/lisa.ps1
@@ -1,5 +1,3 @@
 #!/usr/bin/env pwsh
 $env:LISA_PREFIX = "$env:USERPROFILE\.listenai\lisa"
-$env:Path = "$env:LISA_PREFIX;$env:Path"
-$env:npm_config_prefix = "$env:LISA_PREFIX"
 & "$env:LISA_PREFIX\node.exe" "$env:LISA_PREFIX\node_modules\@listenai\lisa\bin\run" $args
