ec65856a24bfb6541ba1a7656c75e578415c33bf
xzma <xzma@listenai.com>
2022-03-04 11:01:34 +0800

    [feature]优化init钩子

--- src/hooks/run/event.ts
+++ src/hooks/run/event.ts
@@ -4,21 +4,21 @@ import * as Configstore from 'configstore'
 const config = new Configstore('lisa')
 
 const event = async function () {
-  if (!['login', 'create', '-v', '--version', '-h', '--help'].includes(this.config.version)) {
+  if (!['login', 'create', '-v', '--version', '-h', '--help'].includes(this.config.id)) {
     const allArgv = JSON.parse(JSON.stringify(argv()))
     const allArguments = allArgv._.filter((item, index) => index !== 0)
     delete allArgv._
     const accessToken = config.get('userInfo')?.accessToken
     const params = {
       event_type: 'lisa_action',
-      command: this.config.version,
+      command: this.config.id,
       arguments: JSON.stringify(allArguments),
       flags: JSON.stringify(allArgv),
       source: 'lisa',
     }
     if (accessToken) {
       try {
-        this.debug('>>> 获取request [%s]：%s (%s)', this.config.version, JSON.stringify(params), accessToken)
+        this.debug('>>> 获取request [%s]：%s (%s)', this.config.id, JSON.stringify(params), accessToken)
         const response = await got.post('https://open.listenai.com/event_upload', {
           timeout: 1000,
           headers: {
