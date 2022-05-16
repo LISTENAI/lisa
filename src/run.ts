4ea5786ddba201ed1963e84582de73e002cdb8d3
zhaozhuobin <350243725@qq.com>
2022-05-16 17:34:57 +0800

    fix run.ts格式

--- src/run.ts
+++ src/run.ts
@@ -3,38 +3,39 @@ if (process && process.argv.indexOf('--debug') >= 0) {
   process.env.DEBUG = '*'
   process.argv = process.argv.filter(item => item !== '--debug')
 }
-import { Command } from '@oclif/command'
-import { platform } from 'os'
-if (process.env.LISA_PREFIX) {
-  const oldPath = process.env[findPathKey()]
-  let newPath = []
-  switch(platform()) {
-    case 'win32':
-      newPath = oldPath.split(';').filter(item => {
-        // 保留git和系统相关Path
-        return item.toLowerCase().indexOf('git') > 0 || item.toLowerCase().indexOf('windows') > 0
-      })
-      newPath.push(process.env.LISA_PREFIX)
-      process.env[findPathKey()] = `${newPath.join(';')}`
-      delete process.env['HOME']
-      break
-    case 'darwin':
-    case 'linux':
-      newPath.push(`${process.env.LISA_PREFIX}/libexec`)
-      newPath.push(`${process.env.LISA_PREFIX}/bin`)
-      newPath = newPath.concat(oldPath)
-      process.env[findPathKey()] = `${newPath.join(':')}`
-      break
-  }
-}
+import {Command} from '@oclif/command'
+import {platform} from 'os'
 
 function findPathKey(): string {
   for (const key in process.env) {
-    if (key.toLowerCase() == 'path') {
+    if (key.toLowerCase() === 'path') {
       return key
     }
   }
   return 'PATH'
+}
+
+if (process.env.LISA_PREFIX) {
+  const oldPath = process.env[findPathKey()]
+  let newPath = []
+  switch (platform()) {
+  case 'win32':
+    newPath = oldPath.split(';').filter(item => {
+      // 保留git和系统相关Path
+      return item.toLowerCase().indexOf('git') > 0 || item.toLowerCase().indexOf('windows') > 0
+    })
+    newPath.push(process.env.LISA_PREFIX)
+    process.env[findPathKey()] = `${newPath.join(';')}`
+    delete process.env.HOME
+    break
+  case 'darwin':
+  case 'linux':
+    newPath.push(`${process.env.LISA_PREFIX}/libexec`)
+    newPath.push(`${process.env.LISA_PREFIX}/bin`)
+    newPath = newPath.concat(oldPath)
+    process.env[findPathKey()] = `${newPath.join(':')}`
+    break
+  }
 }
 
 const Sentry = require('@sentry/node')
@@ -51,31 +52,28 @@ class BeforeRunCommand extends Command {
 }
 BeforeRunCommand.run()
 
-
-
 require('@oclif/command').run()
-  .then(require('@oclif/command/flush'))
-  .catch(async (error) => {
-    const oclifHandler = require('@oclif/errors/handle')
-    // do any extra work with error
-    const Configstore = require('configstore')
-    const config = new Configstore('lisa')
-    const lisaUserInfo = config.get('userInfo') || {}
+.then(require('@oclif/command/flush'))
+.catch(async error => {
+  // eslint-disable-next-line node/no-extraneous-require
+  const oclifHandler = require('@oclif/errors/handle')
+  // do any extra work with error
+  const Configstore = require('configstore')
+  const config = new Configstore('lisa')
+  const lisaUserInfo = config.get('userInfo') || {}
 
-    await new Promise<void>((res, _rej) => {
-      Sentry.withScope(function (scope) {
-        scope.setTag('lisaVersion', config.get('version') || '');
-        scope.setTag('userid', lisaUserInfo.id || '');
-        scope.setTag('username', lisaUserInfo.username || '');
-        scope.setTag('accountName', lisaUserInfo.accountName || '');
-        scope.setTag('email', lisaUserInfo.email || '');
-        scope.setTag('accessToken', lisaUserInfo.accessToken || '');
-        Sentry.captureException(error, () => {
-          res()
-        });
-      });
+  await new Promise<void>((res, _rej) => {
+    Sentry.withScope(function (scope) {
+      scope.setTag('lisaVersion', config.get('version') || '')
+      scope.setTag('userid', lisaUserInfo.id || '')
+      scope.setTag('username', lisaUserInfo.username || '')
+      scope.setTag('accountName', lisaUserInfo.accountName || '')
+      scope.setTag('email', lisaUserInfo.email || '')
+      scope.setTag('accessToken', lisaUserInfo.accessToken || '')
+      Sentry.captureException(error, () => {
+        res()
+      })
     })
-    return oclifHandler(error)
   })
-
-
+  return oclifHandler(error)
+})
