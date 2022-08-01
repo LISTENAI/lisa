efc262326debc607f7a5db3ecca2fad7a124b318
xychen <xychen@listenai.com>
2022-08-01 12:06:33 +0800

    优化自动补全逻辑，避免不必要的 listPlugins

--- src/completion.ts
+++ src/completion.ts
@@ -2,35 +2,31 @@
 import lisa from '@listenai/lisa_core';
 import { join } from 'path';
 import * as tabtab from 'tabtab';
-import { IPluginCompletion, IPluginInfo, listPlugins } from './util/plugins';
+import { getPluginByFriendlyName, IPluginCompletion, IPluginInfo, listPlugins } from './util/plugins';
 
 (async () => {
   const env = tabtab.parseEnv(process.env);
   if (!env.complete) return;
 
-  const plugins: Record<string, IPluginInfo> = (await listPlugins())
-    .reduce((plugins, plugin) => {
-      plugins[plugin.friendlyName] = plugin;
-      return plugins;
-    }, {});
-
   if (env.words == 1) {
-    return tabtab.log(Object.values(plugins).map((plugin) => ({
+    return tabtab.log((await listPlugins()).map((plugin) => ({
       name: plugin.friendlyName,
       description: plugin.name,
     })));
   }
 
   const match = env.line.match(/^lisa (\S+) /);
-  if (match && plugins[match[1]] && plugins[match[1]].package.lisa?.completion) {
-    const plugin = plugins[match[1]];
-    const completionScript = join(plugin.root, plugin.package.lisa.completion);
-    if (await lisa.fs.pathExists(completionScript)) {
-      const completionModule = await import(completionScript);
-      const completionFn = completionModule.default as IPluginCompletion;
-      const completion = typeof completionFn == 'function' && await completionFn(env);
-      if (completion) {
-        return tabtab.log(completion);
+  if (match) {
+    const plugin = await getPluginByFriendlyName(match[1]);
+    if (plugin?.package.lisa?.completion) {
+      const completionScript = join(plugin.root, plugin.package.lisa.completion);
+      if (await lisa.fs.pathExists(completionScript)) {
+        const completionModule = await import(completionScript);
+        const completionFn = completionModule.default as IPluginCompletion;
+        const completion = typeof completionFn == 'function' && await completionFn(env);
+        if (completion) {
+          return tabtab.log(completion);
+        }
       }
     }
   }
