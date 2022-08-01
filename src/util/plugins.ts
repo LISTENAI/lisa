d0758e526a989c97988a6c99b1d6b1b612585f22
xychen <xychen@listenai.com>
2022-08-01 11:28:47 +0800

    梳理加载插件信息的流程

--- src/util/plugins.ts
+++ src/util/plugins.ts
@@ -1,5 +1,5 @@
 import lisa from '@listenai/lisa_core';
-import { join, resolve } from 'path';
+import { basename, join, resolve } from 'path';
 import { CompletionItem, TabtabEnv } from 'tabtab';
 
 const CACHE_DIR = resolve(__dirname, '..', '..', 'var');
@@ -15,7 +15,8 @@ export interface IPluginInfo {
 }
 
 export interface IPluginPackageInfo {
-  version?: string;
+  name: string;
+  version: string;
   engines?: Record<string, string>;
   main?: string;
   lisa?: {
@@ -32,42 +33,50 @@ export interface IPluginCompletion {
   (env: TabtabEnv): Promise<CompletionItem[] | string[] | undefined>;
 }
 
+async function loadPluginInfo(pluginDir: string): Promise<IPluginInfo | undefined> {
+  const pkgFile = join(pluginDir, 'package.json');
+  if (await lisa.fs.pathExists(pkgFile)) {
+    const pkg: IPluginPackageInfo = await lisa.fs.readJson(pkgFile);
+    return {
+      name: pkg.name,
+      friendlyName: pkg.lisa?.friendlyName || basename(pkg.name),
+      version: pkg.version,
+      root: pluginDir,
+      package: pkg,
+    };
+  } else {
+    return undefined;
+  }
+}
+
 export async function listPlugins(): Promise<IPluginInfo[]> {
   const pluginsRoot = await getPluginsRoot();
+  lisa.application.debug('listPlugins start');
   if (await lisa.fs.pathExists(pluginsRoot)) {
     const plugins = <IPluginInfo[]>[];
     for (const dir of await lisa.fs.readdir(pluginsRoot)) {
-      const pkgFile = join(pluginsRoot, dir, 'package.json');
-      if (await lisa.fs.pathExists(pkgFile)) {
-        const pkg: IPluginPackageInfo = await lisa.fs.readJson(pkgFile);
-        plugins.push({
-          name: `${SCOPE_NAME}/${dir}`,
-          friendlyName: pkg.lisa?.friendlyName || dir,
-          version: pkg.version,
-          root: join(pluginsRoot, dir),
-          package: pkg,
-        });
+      const pluginInfo = await loadPluginInfo(join(pluginsRoot, dir));
+      if (pluginInfo) {
+        plugins.push(pluginInfo);
       }
     }
+    lisa.application.debug(`listPlugins done, resolved ${plugins.length} plugins`);
     return plugins;
   } else {
+    lisa.application.debug(`listPlugins done, no plugins`);
     return [];
   }
 }
 
 export async function getPlugin(id: string): Promise<IPluginInfo | undefined> {
   const pluginsRoot = await getPluginsRoot();
-  const pkgFile = join(pluginsRoot, id, 'package.json');
-  if (await lisa.fs.pathExists(pkgFile)) {
-    const pkg: IPluginPackageInfo = await lisa.fs.readJson(pkgFile);
-    return {
-      name: `${SCOPE_NAME}/${id}`,
-      friendlyName: pkg.lisa?.friendlyName || id,
-      version: pkg.version,
-      root: join(pluginsRoot, id),
-      package: pkg,
-    };
+  lisa.application.debug(`getPlugin(${id}) start`);
+  const pluginInfo = await loadPluginInfo(join(pluginsRoot, id));
+  if (pluginInfo) {
+    lisa.application.debug(`getPlugin(${id}) done, resolved`);
+    return pluginInfo;
   } else {
+    lisa.application.debug(`getPlugin(${id}) done, not found`);
     return undefined;
   }
 }
