3bb895c12d6f67cdd2ebc56e8c75efebc5991493
jameswang <105700413+listenai-jw@users.noreply.github.com>
2022-11-24 15:54:01 +0800

    (add) download and apply eup

--- src/commands/update.ts
+++ src/commands/update.ts
@@ -2,6 +2,8 @@ import {Command} from '@oclif/command'
 import lisa from '@listenai/lisa_core'
 import compare from '../util/compare'
 import lpminit from '../util/lpminit'
+import download from '@xingrz/download2';
+import {resolve} from "path";
 
 export default class Update extends Command {
   static description = '更新lisa到最新版本'
@@ -45,8 +47,10 @@ export default class Update extends Command {
       command = ['view', wantUpdatePkg, 'dist-tags']
       const res = await cmd('npm', registry ? command.concat(registry) : command)
       const distTags = JSON.parse(res.stdout.replace(/(\s*?{\s*?|\s*?,\s*?)(['"])?([a-zA-Z0-9]+)(['"])?:/g, '$1"$3":').replace(/'/g, '"'))
-      //const channelLatestVersion: string = channel === 'beta' ? distTags.beta : distTags.latest;
-      const channelLatestVersion: string = '1.6.9';
+      let channelLatestVersion: string = channel === 'beta' ? distTags.beta : distTags.latest;
+      if (process.env.LISA_DUMMY_VERSION) {
+        channelLatestVersion = process.env.LISA_DUMMY_VERSION;
+      }
       this.debug('最新版本: %s, 通道: %s', channelLatestVersion, channel)
 
       //check if current version
@@ -54,11 +58,41 @@ export default class Update extends Command {
         return cli.action.stop(`已是最新版本 ${currentVersion}`)
       }
 
-      //check for express package
-      this.debug('Checking for express update package...')
+      //check if eup is available
+      let eupUrl = '';
+      this.debug('Checking for EUP...')
       const isBeta: Number = channel === 'beta' ? 1 : 0;
-      const expressUpdateResult = await this.getExpressUpdatePackageInfo(wantUpdatePkg, channelLatestVersion, isBeta);
-      return cli.action.stop(JSON.stringify(expressUpdateResult));
+      const eupResult = await this.getEUPInfo(wantUpdatePkg, channelLatestVersion, isBeta);
+      if (eupResult.recode === '000000') {
+        if (compare(currentVersion, eupResult.data.expressBaseVersion) >= 0) {
+          this.debug(`EUP available! ver = ${eupResult.data.version}, baseVer = ${eupResult.data.expressBaseVersion}`);
+          eupUrl = eupResult.data.expressPackageUrl.replace(".7z", ".zip");
+        } else {
+          this.debug(`EUP available but current version does not meet base version requirement. ver = ${eupResult.data.version}, baseVer = ${eupResult.data.expressBaseVersion}`);
+        }
+      } else {
+        this.debug(`EUP unavailable! code = ${eupResult.recode}, msg = ${eupResult.desc}`)
+      }
+
+      if (eupUrl.length > 'https://'.length) {
+        //if eup is available, download it and install
+        cli.action.stop(`开始更新${wantUpdatePkg}@${channelLatestVersion} (EUP)`);
+        cli.action.start('正在下载与部署');
+        const pluginPath = await this.getPluginPath(wantUpdatePkg);
+        await download(eupUrl, pluginPath, {
+          extract: true
+        });
+        cli.action.stop('完成！');
+      } else {
+        //or, run npm things
+        cli.action.stop(`开始更新${wantUpdatePkg}@${channelLatestVersion} (NPM)`)
+        command = ['install', `${wantUpdatePkg}@${channelLatestVersion}`, '-g']
+        await cmd('npm', registry ? command.concat(registry) : command, {
+          shell: true,
+          stdio: 'inherit',
+        })
+        this.log('成功')
+      }
     } catch (error) {
       throw error
     }
@@ -70,15 +104,15 @@ export default class Update extends Command {
     }
 
     const {cmd} = lisa
-    const query = [ 'ls', pluginName, '--json' ];
+    const query = [ 'ls', pluginName, '--json', '-g' ];
     const resultRaw = (await cmd('npm', query)).stdout;
     const result = JSON.parse(resultRaw);
 
     return result['dependencies'][pluginName]['version'] ?? '-.-.-';
   }
 
-  async getExpressUpdatePackageInfo(name: string, expectedVersion: string, isBeta: Number) {
-    const {cli, got} = lisa
+  async getEUPInfo(name: string, expectedVersion: string, isBeta: Number) {
+    const {got} = lisa
     let result: any = {};
 
     const requestUrl = `${Update.CASTOR_URL_PREFIX}/lisaPlugin/version?name=${name}&version=${expectedVersion}&isBeta=${isBeta}`;
@@ -99,6 +133,10 @@ export default class Update extends Command {
     } finally {
       return result
     }
+  }
+
+  async getPluginPath(name: string) {
+    return resolve((process.env.LISA_HOME || ""), 'lisa', 'node_modules', name);
   }
 
   /*async run() {
